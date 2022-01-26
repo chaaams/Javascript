86e2e05a42f1446562d71e311013e85872307ced
chaaams <diaby.chamssoudine@gmail.com>
2022-01-26 14:48:04 +0100

    javascript day 2

--- jour-2/jour-2.js
+++ jour-2/jour-2.js
@@ -1,18 +1,67 @@
 // 01 Number
+
 var integer = 102;
 var float = 13.9;
-console.log(integer,float)
+console.log(integer,float);
 
-// 02
+// 02 Convert
 
-// 03
+var basic = 34;
+var stringfield = basic.toString();
+console.log(stringfield);
 
-// 04
+// 03 Round
 
-// 05
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
 
-// 06
+// 04 Arithmétique
 
-// 07
+var test = 12;
+var bis = 5;
+console.log("12+5 = ",test+bis);
+console.log("12-5 = ",test-bis);
+console.log("12*5 = ",test*bis);
+console.log("12/5 = ",test/bis);
+console.log("12 ** 5 = ",test**bis);
+console.log("12 % 5 = ",test%bis);
 
-// 08
+// 05 Comparaison
+
+var test = 143;
+var bis = 219;
+console.log("143 > 219 =",test > bis)
+console.log("143 < 219 =",test < bis)
+console.log("143 >= 219 =",test >= bis)
+console.log("143 <= 219 =",test <= bis)
+console.log("143 == 219 =",test == bis)
+console.log("143 === 219 =",test === bis)
+console.log("143 != 219 =",test != bis)
+console.log("143 !== 219 =",test !== bis)
+
+
+// 06 Condition
+
+var limit = 50;
+var score = 64;
+
+if(score >= limit){
+    console.log("Ok good!");
+}else{
+    console.log("Oh nooo...");
+}
+
+
+// 07 Condition II
+
+var password = "azerty"
+
+if(password.length > 5){
+    console.log("The password is secure");
+}else{
+    console.log("The password is not secure");
+}
+
+
+// 08 Condition III
