98011291596cb0608f057b854025957a9c062cda
chaaams <diaby.chamssoudine@gmail.com>
2022-01-27 21:17:59 +0100

    Javascript day 3

--- jour-3/jour-3.js
+++ jour-3/jour-3.js
@@ -1,4 +1,21 @@
 // 01  Array
 
 var fruits = ["mango","lemon","blueberry"];
-console.log(fruits)
+console.log("01 Array --> ",fruits);
+
+// 02 Acces
+
+var ingredients = ["eggs","milk","butter"];
+console.log("02 Acces --> ",ingredients[1]);
+console.log("02 Acces --> ",ingredients[2]);
+
+// 03 Add and Remove
+
+var object = ["pen","book","lamp"];
+console.log("03 Add and Remove --> ",object);
+object.unshift("chair");
+console.log("03 Add and Remove --> ",object);
+object.pop("lamp");
+console.log("03 Add and Remove --> ",object);
+object.push("laptop");
+console.log("03 Add and Remove --> ",object);
