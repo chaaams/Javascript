c6c40bbf1de21625290418562a587f6ecfa4d283
chaaams <diaby.chamssoudine@gmail.com>
2022-01-28 10:56:53 +0100

    Javascript day 4

---
+++ jour-4/jour-4.js
@@ -0,0 +1,14 @@
+// 01 Object
+var cat ={
+    name:'Garfield',
+    age: 3,
+    isCute: true,
+}
+console.log("01 Object --> ",cat);
+console.log("01 Object --> ",cat.age);
+if(cat.isCute === true){
+    console.log("So Cute");
+}
+
+// 02 Combine
+
