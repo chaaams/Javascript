1647f53af8b553a48cc08aa2b51cc65f60b327ea
chaaams <diaby.chamssoudine@gmail.com>
2022-01-28 10:37:22 +0100

    Javascript day 3

--- jour-3/jour-3.js
+++ jour-3/jour-3.js
@@ -19,3 +19,48 @@ object.pop("lamp");
 console.log("03 Add and Remove --> ",object);
 object.push("laptop");
 console.log("03 Add and Remove --> ",object);
+object.shift();
+console.log("03 Add and Remove --> ",object);
+
+// 04 Order
+
+var numbers = [4,10,8,12,6]
+console.log("04 Order --> ",numbers);
+numbers.reverse();
+console.log("04 Order --> ",numbers);
+numbers.sort((a, b) => a - b);
+console.log("04 Order --> ",numbers);
+
+// 05 Boucle
+
+var total = 0;
+var limit = 10;
+console.log("05 Boucle --> ",total,limit);
+var result = 0;
+var i = 0;
+for(i = 0; i <= limit; i++){
+    total = total + i ;
+}
+console.log("05 Boucle --> ",total);
+
+// 06 Reverse
+
+var sentence = "Hello Konexio !";
+console.log("06 Reverse --> ",sentence)
+var result = "";
+for(var i = sentence.length-1; i >= 0; i--){
+    result = result + sentence[i];
+}
+console.log("06 Reverse --> ",result);
+
+// Bonus
+
+for(var i = 0; i <= 100; i++){
+    if(i % 3===0){
+        console.log("Bonus --> Fizz")
+    }
+    if(i % 5 === 0){
+        console.log("Bonus --> buzz")
+    }
+    
+}
