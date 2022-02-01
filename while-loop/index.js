c23835ab4be1f2ecd42e66703e11b57812fcf988
chaaams <diaby.chamssoudine@gmail.com>
2022-02-01 16:37:14 +0100

    Loop while exercice

--- while-loop/index.js
+++ while-loop/index.js
@@ -17,3 +17,21 @@ while(dice <= 6){
     count++;
 }
 console.log("02 Try again --> ",count)
+
+// 03 Course
+
+let usainBolt = 0;
+let tysonGay = 0;
+while(usainBolt <= 100 && tysonGay <= 100){
+    usainBolt += Math.ceil(Math.random()*10);
+    tysonGay += Math.ceil(Math.random()*10);
+}
+if(usainBolt > 100){
+    console.log("Usain is the winner");
+}
+else if(tysonGay > 100){
+    console.log("Tyson is the winner")
+}
+else{
+    console.log("no one win")
+}
