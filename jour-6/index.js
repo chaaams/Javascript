a1ca950f3061214b61e050a153ad6a853cf7ea82
chaaams <diaby.chamssoudine@gmail.com>
2022-02-02 15:49:03 +0100

    Javascript day 6

--- jour-6/index.js
+++ jour-6/index.js
@@ -4,8 +4,8 @@ function formatDate(dateString) {
 
     var date = new Date(dateString);
 
-    var jj = date.getDate().toString().padStart(2, "0");
-    var mm = (date.getMonth() + 1).toString().padStart(2, "0");
+    var jj = date.getDate().toString().padStart(2, "0"); // récupère le jour de la date en string et un padstart de 2
+    var mm = (date.getMonth() + 1).toString().padStart(2, "0"); // récupère le mois de la date en string et un padstart de 2
 
     console.log(`Result: ${jj}/${mm}/${date.getFullYear()}`)
 
@@ -13,3 +13,11 @@ function formatDate(dateString) {
 
 formatDate("1977-12-15");
 
+// Age
+
+function calculateAge(dateBirth){
+    var date = new Date(dateBirth);
+    var age = new Date (new Date() - date);
+    return age.getFullYear() - 1970;
+}
+console.log("02 Age --> ",calculateAge("1996-08-03"))
