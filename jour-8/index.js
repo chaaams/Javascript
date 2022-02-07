185307c9315d56f220a0507e65d05cac39e77239
chaaams <diaby.chamssoudine@gmail.com>
2022-02-07 16:24:22 +0100

    javascript day 8 exercices

--- jour-8/index.js
+++ jour-8/index.js
@@ -2,17 +2,33 @@
 
 const request = require('request');
 
-const apiBase = 'https://restcountries.com/v3.1/all';
-const countryNames = [];
+// const apiBase = 'https://restcountries.com/v3.1/all';
+// const countryNames = [];
 
-request.get(apiBase, function(err,res,body){
-    if(err){
-        console.log(err);
-        return;
-    }
-     countries = JSON.parse(body).map(function(country){
-        return country.name.common
-     });
-     console.log(countries.join(" - "))
+// request.get(apiBase, function(err,res,body){
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//      countries = JSON.parse(body).map(function(country){
+//         return country.name.common
+//      });
+//      console.log(countries.join(" - "))
+// })
 
-})
+
+// 02 - Chuck Norris
+
+const apiBase = 'https://api.chucknorris.io/jokes/random';
+function getFact(){
+    request.get(apiBase, function(err,res,body){
+        if (err){
+            console.log(err);
+            return;
+        }
+        let fact = JSON.parse(body)
+        console.log(fact.value)
+    })
+
+}
+getFact();
